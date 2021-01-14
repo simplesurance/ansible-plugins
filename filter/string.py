e67a1b735d9b3479afb81b3323c1d082774d57b9
Jose' Pedro Saraiva <js@simplesurance.de>
2021-01-14 12:55:29 +0100

    [ADHOC] Add new to_escaped_json as an alternative to to_safe_json without extra double quotes

--- filter/string.py
+++ filter/string.py
@@ -3,15 +3,26 @@ import json
 
 def to_safe_json(value):
     """
-    Safely escapes a json string so that it can be wrapped in double quotes.
+    Safely escapes a json string and wraps it in double quotes.
     This is useful to use in deploy vars so that they can be safely passed
     as hcl environment variables.
     """
     return json.dumps(json.dumps(value))
 
+def to_escaped_json(value):
+    """
+    Safely escapes a json string so that it can be wrapped in double quotes.
+    This is useful to use in deploy vars so that they can be safely passed
+    as hcl environment variables.
+    The difference between `to_safe_json` and `to_escaped_json` is that
+    `to_escaped_json` does NOT include the extra double quotes around the
+    whole escaped json string.
+    """
+    return json.dumps(json.dumps(value))[1:-1]
 
 class FilterModule(object):
     def filters(self):
         return {
             'to_safe_json': to_safe_json,
+            'to_escaped_json': to_escaped_json,
         }
